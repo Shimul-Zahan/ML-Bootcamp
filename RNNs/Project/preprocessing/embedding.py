de03b0d23c499d1d590fb7e07f3651675d7e9017
Xing Ping <112884750+Shimul-Zahan@users.noreply.github.com>
2025-03-22 14:11:25 +0600

    Updates all for ending

---
+++ RNNs/Project/preprocessing/embedding.py
@@ -0,0 +1,13 @@
+from tensorflow.keras.preprocessing.text import one_hot
+
+### sentences
+sent=['the glass of milk',
+     'the glass of juice',
+     'the cup of tea',
+    'I am a good boy',
+     'I am a good developer',
+     'understand the meaning of words',
+     'your videos are good',]
+
+
+print("Hello world")
